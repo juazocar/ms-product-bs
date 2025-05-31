b19bee1158dd835187912a75956ebfc066b2633e
Plaza Vespucio <plazavespucio@PVAWSB03LC1100.duoc.acad>
2025-05-30 20:46:38 -0400

    first commit

---
+++ src/main/java/cl/duoc/ms_product_bs/MsProductBsApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_product_bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductBsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/ms_product_bs/MsProductBsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_product_bs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
